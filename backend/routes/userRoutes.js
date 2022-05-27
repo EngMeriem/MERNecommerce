aab3daf50eec9ff985a955cc1ba388306fc4efd5
Meriem <meriem.ayed.tn@gmail.com>
2022-05-27 15:11:30 +0100

    update userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -111,23 +111,26 @@ userRouter.put(
   isAuth,
   expressAsyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
+
     if (user) {
       user.name = req.body.name || user.name;
       user.email = req.body.email || user.email;
       if (req.body.password) {
-        user.password = bcrypt.hashSync(req.body.password);
+        user.password = req.body.password;
       }
 
       const updatedUser = await user.save();
-      res.send({
+
+      res.json({
         _id: updatedUser._id,
         name: updatedUser.name,
         email: updatedUser.email,
         isAdmin: updatedUser.isAdmin,
-        token: generateToken(updatedUser),
+        token: generateToken(updatedUser._id),
       });
     } else {
-      res.status(404).send({ message: 'User not found' });
+      res.status(404);
+      throw new Error('User not found');
     }
   })
 );
